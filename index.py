797ddbf1517ab5bac4a057d05b649a67656df68c
danatael <li_davidcruz@unca.edu.mx>
2025-10-08 09:20:15 -0600

    fix: Rediseño de plataforma

--- index.py
+++ index.py
@@ -8,4 +8,4 @@ from Controllers.index import main_controller
 app.register_blueprint(main_controller)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
